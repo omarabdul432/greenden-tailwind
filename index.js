8ef832b11085e764e8ac3043c4bed80f29673f9c
omarabdul432 <abdomar0748@gmail.com>
2024-03-12 13:38:44 +0530

    updated javascript functionality

---
+++ index.js
@@ -0,0 +1,15 @@
+// selecting sidenav and menun icon
+
+var sidenav = document.getElementById("sidenav")
+var menuicon = document.getElementById("menuicon")
+
+
+menuicon.addEventListener("click", function () {
+    sidenav.style.right = 0
+})
+
+var closenav = document.getElementById("closenav")
+
+closenav.addEventListener("click", function () {
+    sidenav.style.right = "-50%"
+})
